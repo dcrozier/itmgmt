3f2c6066053e9620c78c68a64e5095cf554ac0f0
don <donald@VXSMON.VXMASTER.COM>
2016-03-16 17:40:15 -0700

    cleanup

--- pt_extreme/minions/models.py
+++ pt_extreme/minions/models.py
@@ -2,5 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Minion(models.Model):
-    
+    mac_address = models.CharField(primary_key=True, max_length=17)
+    public_ip = models.IPAddressField()
+    private_ip = models.IPAddressField()
+    host_name = models.CharField()
+
+    def __unicode__(self):
+        return self.host_name
